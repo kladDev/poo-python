8d61b0eecc32a07fa8a0d1eb06789de1d08c3ae8
Antonio Claudio <87538025+kladDev@users.noreply.github.com>
2022-09-12 20:28:09 -0300

    Update maior_menor.py

--- lista03/maior_menor.py
+++ lista03/maior_menor.py
@@ -3,19 +3,19 @@
 # Escreva um programa que leia 5 números e escreva o menor valor lido
 # e o maior valor lido
 
-for i in range(0, 5, 1):
-    valores = float(input(f"Digite o {i+1}° valor: "))
+for i in range(5):
+    value = float(input(f"Digite o {i+1}° valor: "))
 
     if i == 0:
-        menor_valor = valores
-        maior_valor = valores
+        lower_value = value
+        bigger_value = value
 
-    if menor_valor < valores:
-        menor_valor = valores
+    if lower_value < value:
+        lower_value = value
 
-    if maior_valor > valores:
-        maior_valor = valores
+    if bigger_value > value:
+        bigger_value = value
 
 print("")
-print(f"O maior valor é {maior_valor:.2f}")
-print(f"O menor valor é {menor_valor:.2f}")
+print(f"O maior valor é {bigger_value:.2f}")
+print(f"O menor valor é {lower_value:.2f}")
