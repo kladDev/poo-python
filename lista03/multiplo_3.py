15a6a033c9210f592cedb9a550c0da43691ca300
Antonio Claudio <87538025+kladDev@users.noreply.github.com>
2022-09-18 18:59:11 -0300

    Update multiplo_3.py

--- lista03/multiplo_3.py
+++ lista03/multiplo_3.py
@@ -3,14 +3,14 @@
 # Faça um programa que determine e mostre os cinco primeiros
 # múltiplos de 3, considerando números maiores que 0
 
-multiplo_3 = 0
+multiple_3 = 0
 i = 0
 
-while multiplo_3 <= 5:
+while multiple_3 <= 5:
     i += 1
     if i % 3 == 0:
         print(i)
-        multiplo_3 += 1
+        multiple_3 += 1
 
 
 
