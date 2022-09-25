49ea1378f5694b79407473cc58835429b73939b8
Antonio Claudio <87538025+kladDev@users.noreply.github.com>
2022-09-25 19:59:59 -0300

    Update notas.py

--- lista05/notas.py
+++ lista05/notas.py
@@ -6,8 +6,8 @@
 def separador():
     print("-"*25)
 
-notas = []
-soma = 0
+grades = []
+sum = 0
 
 separador()
 print("Sistema de notas escolar")
@@ -15,9 +15,9 @@ separador()
 print("Digite 4 notas abaixo")
 
 for i in range(0, 4, 1):
-    notas.append(float(input(f"Nota 0{i + 1}: ")))
-    soma += notas[i]
+    grades.append(float(input(f"Nota 0{i + 1}: ")))
+    sum += grades[i]
 
 print("")
-print(notas)
-print(f"A média é {soma/len(notas)}")
+print(grades)
+print(f"A média é {sum/len(grades)}")
