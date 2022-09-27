6c3a3cdef12e83ac236f75cabdee164e36579e86
Antonio Claudio <87538025+kladDev@users.noreply.github.com>
2022-09-27 10:17:22 -0300

    Update dobro.py

--- lista06/dobro.py
+++ lista06/dobro.py
@@ -6,13 +6,13 @@
 def separador():
     print("-"*15)
 
-def dobro(valor):
-    return valor * 2
+def double(value):
+    return value * 2
 
 separador()
 print("\tDOBRO")
 separador()
 
-valor = int(input("Digite o valor: "))
-valor = dobro(valor)
-print(f"Seu dobro é {valor}")
+value = int(input("Digite o valor: "))
+value = double(value)
+print(f"Seu dobro é {value}")
