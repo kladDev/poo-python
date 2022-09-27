a072f7d44c3d65d7dde7e1d732b11235c93b91e5
Antonio Claudio <87538025+kladDev@users.noreply.github.com>
2022-09-27 10:20:11 -0300

    Update maior_valor.py

--- lista06/maior_valor.py
+++ lista06/maior_valor.py
@@ -6,20 +6,19 @@
 def separador():
     print("-"*15)
 
-def maior_valor(valor1, valor2):
-    if valor1 > valor2:
-        return valor1
-    elif valor2 > valor1:
-        return valor2
-    else:
-        return valor1
+def biggest_value(first_value, second_value):
+    if first_value > second_value:
+        return first_value
+    else second_value >= first_value:
+        return second_value
+
 
 separador()
 print("MAIOR VALOR")
 separador()
 
-valor1 = float(input("Valor 01: "))
-valor2 = float(input("Valor 02: "))
-maior = maior_valor(valor1, valor2)
+first_value = float(input("Valor 01: "))
+second_value = float(input("Valor 02: "))
+bigger = biggest_value(first_value, second_value)
 
-print(f"Foram lidos {valor1:.2f} e {valor2:.2f}\nO maior valor é {maior:.2f}")
+print(f"Foram lidos {first_value:.2f} e {second_value:.2f}\nO maior valor é {bigger:.2f}")
